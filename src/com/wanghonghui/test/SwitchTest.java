0ab4a4b91b19ee151782f20b664563b742775c9e
wanghonghui <wanghonghui_work@163.com>
2020-04-04 17:40:46 +0800

    -  addOn Lambda Stream 学习

---
+++ src/com/wanghonghui/test/SwitchTest.java
@@ -0,0 +1,22 @@
+package com.wanghonghui.test;
+
+public class SwitchTest {
+    public static void main(String[] args) {
+        method(null);
+    }
+    public static void method(String param) {
+        switch (param) {
+            // 肯定不是进入这里
+            case "sth":
+                System.out.println("it's sth");
+                break;
+            // 也不是进入这里
+            case "null":
+                System.out.println("it's null");
+                break;
+            // 也不是进入这里
+            default:
+                System.out.println("default");
+        }
+    }
+}
